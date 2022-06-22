a17d4abc9cddc2a675c1ca83a5b51a5922ecfe96
Arpit Dubey <aarpitdubey@gmail.com>
2022-06-22 17:24:18 +0530

    Updated

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 # Declaring variables for setup function
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.1"
+VERSION="0.0.3"
 AUTHOR="Arpit Dubey"
 DESCRIPTION="This is the first ML Project for FSDS Nov 2021 batch"
 PACKAGES=["housing"]
@@ -12,6 +12,8 @@ REQUIREMENT_FILE_NAME="requirements.txt"
 
 def get_requirements_list()->List[str]:
     """
+    ["numpy", "pandas"] -> List[str]
+
     Description: This function is going to return list of requirement
     mention in requirements.txt file
 
@@ -19,14 +21,14 @@ def get_requirements_list()->List[str]:
     of libraries mentioned in reuirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup (
     name=PROJECT_NAME,
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(), #PACKAGES
     install_requires=get_requirements_list()
 
 )
